fd429cbcb7e41f00140ec82309ceec5c781f4670
Stevek <77753228+Stevekk11@users.noreply.github.com>
2024-04-14 16:01:15 +0200

    added line counter

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,27 @@
 package main;
 
 import javax.swing.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Gallery g = new Gallery();
-
+        SwingUtilities.invokeLater(() -> {
+            try {
+                linesOfCode();
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
             }
         });
+    }
+
+    public static void linesOfCode() throws IOException, InterruptedException {
+        String command = "cloc.exe";
+        ProcessBuilder pb = new ProcessBuilder(command, "src");
+        Process process = pb.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
     }
 }
